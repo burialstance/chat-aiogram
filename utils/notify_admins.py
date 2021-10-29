d3c31747c4b7749802ffa6fbd06b1857e37f89bf
burialstance <burialstance@gmail.com>
2021-10-29 15:35:54 +0500

    change parse_mode to HTML

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,20 +1,18 @@
 from loguru import logger
 from aiogram import Dispatcher
 from aiogram.types import ParseMode
-from aiogram.utils.markdown import text, bold, code
-
 
 from config import TELEGRAM_ADMINS
 
 
 async def notify_admins(dispatcher: Dispatcher, message: str, header='NOTIFY SYSTEM'):
     notify = '\n'.join([
-        text(code(header)),
-        text(bold(message))
+        f"<code>{header}</code>",
+        f"<b>{message}</b>",
     ])
 
     for admin_id in TELEGRAM_ADMINS:
         try:
-            await dispatcher.bot.send_message(admin_id, notify, parse_mode=ParseMode.MARKDOWN)
+            await dispatcher.bot.send_message(admin_id, notify, parse_mode=ParseMode.HTML)
         except Exception as err:
             logger.exception(err)
